302ed15b658a8d4dbfada887554160397c441886
Silvio Rodrigues Correia <silviorodriguescorreia@outlook.com>
2023-10-06 22:05:23 -0300

    Aula 1

---
+++ src/javascript/1.js
@@ -0,0 +1,22 @@
+// Iniciando o ponteiro de memoria
+// Olá, mundo!
+//string nome;
+//nome;
+//var nome;
+//nome = 'Olá, mundo!';
+//console.log("TESTE");
+
+var num = 0;
+num = num + 2;
+num = num * 3;
+num = num / 2;
+num++;
+num--;
+num+= 1;
+num-= 2;
+num*= 3;
+num/= 2;
+
+var divElement = document.getElementById("resultado");
+
+divElement.innerHTML = num;
---
+++ src/readme.md
@@ -0,0 +1,11 @@
+# Projeto do site de curso Javascript
+
+## Como executar o projeto
+
+Caso você não posua no seu VSCode o Open in browser é recomendado
+pelo time da turma 1 a instalação do plugin: 
+
+Uma vez instalado você deverá clicar com o botão direito do mouse na
+pasta do projeto e então 
+
+
